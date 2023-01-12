34a889325cbd886d64bf508b1847986955d92cc3
Ppaaoo <62310221+Ppaaoo@users.noreply.github.com>
2023-01-12 10:53:33 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 let inputString = window.prompt("Input a string:");
 
-function sort(string) {
-    let capitalArr = string.match(/[A-Z]/g, '');
-    let lowercaseArr = string.match(/[^A-Z]/g, '');
-    let numberArr = string.match(/\d+/g);
+function sort(iString) {
+    let capitalArr = [];
+    let lowercaseArr = [];
+    let numberArr = [];
+
+    for(let i = 0; i < iString.length; i++) {
+        if(iString[i] == iString[i].toUpperCase() && iString[i] != iString[i].match(/\d/g)) {
+            capitalArr.push(iString[i]);
+        }
+        if(iString[i] == iString[i].toLowerCase() && iString[i] != iString[i].match(/\d/g)) {
+            lowercaseArr.push(iString[i]);
+        }
+        if(iString[i] == iString[i].match(/\d/g)) {
+            numberArr.push(iString[i])
+        }
+    }
 
     capitalArr.push(lowercaseArr.join(''));
     capitalArr.push(numberArr.join(''));
